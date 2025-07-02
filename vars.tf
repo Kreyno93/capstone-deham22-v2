53432254acca922a97e8be39387c65de4e376bfc
Kreyno93 <michael.wagener@neuefische.de>
2025-07-02 10:38:11 +0200

    adding variables for more flexibility

---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string 
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the EC2 instance"
+  type        = string
+}
+
+variable "ami_id_linux" {
+  description = "AMI ID to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
+}
