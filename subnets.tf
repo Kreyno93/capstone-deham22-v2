55c238a10f64c652c9ffb3ba67e32529527d61a8
Kreyno93 <michael.wagener@neuefische.de>
2025-07-02 10:51:05 +0200

    adjusting variables and set new one for subnets

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "my_first_subnet" {
   vpc_id     = aws_vpc.my_first_vpc.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = var.subnet_cidr
 
   tags = {
     Name = "Public Subnet 1"
--- vars.tf
+++ vars.tf
@@ -23,3 +23,9 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
